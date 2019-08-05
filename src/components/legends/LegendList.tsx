c932619ff5077e5bd9cd804e07f6a65eb0047fe1
Robert Sanins <rob.sanins@lovehoney.co.uk>
2019-08-05 17:17:32 +0100

    style icons to have active tabs

--- src/components/legends/LegendList.tsx
+++ src/components/legends/LegendList.tsx
@@ -2,6 +2,8 @@ import React, { useRef, useEffect } from "react";
 import useLegendsService from "./Services/UseLegendsService";
 import { LegendListProps } from '../Types/Types';
 import LegendDetail from './LegendDetails';
+import './LegendList.scss';
+import clsx from "clsx";
 
 const scrollToRef = (ref: any) => window.scrollTo(0, ref.current.offsetTop)   
 const useMountEffect = (fun: any) => useEffect(fun, [])
@@ -14,42 +16,48 @@ const LegendList: React.FC<{}> = () => {
   const myRef = useRef(null)
 
   useMountEffect(() => scrollToRef(myRef)) // Scroll on mount
-
-  const lala = (key:any) => () => {
+  
+  const toggleSelected = (key:any) => () => {
     setActiveKey(key);
-    scrollToRef(myRef)
-	};
+    scrollToRef(myRef);
+  };
   
   return (
     <>
-      <div ref={myRef} className='main'></div> 
-
-      <div>
+      <div ref={myRef} className='main'></div>
+      <div>   
         {service.status === 'loading' && (
           <p>Loading</p>
         )}
-        
+      </div>
+      <div>
         {service.status === 'loaded' &&
-
-        <div>
           <div>
-            {service.payload.map((legends: LegendListProps, key:any) => (
-                <button className='' style={{background: 'blue'}} key={key} onClick={lala(key)}>
-                  <img style={{width: '60px'}} src={require(`./Images/${legends.name}.png`)}/>
-                  {legends.name}
-                </button>
-            ))}
+            <div>
+              {service.payload.map((legends: LegendListProps, key:any) => (
+                <button 
+                  className={clsx(key === activeKey && 'legend-list--button__' + legends.name + '--active', 'legend-list--button legend-list--button__' + legends.name)} 
+                  key={key} 
+                  onClick={toggleSelected(key)}
+                >
+                  <img 
+                    style={{width: '60px'}} 
+                    src={require(`./Images/${legends.name}.png`)}
+                  />
+                  <div className='legend-list__name'>
+                    {legends.name}                    
+                  </div>
+                </button>           
+              ))}
+            </div>
+            <div>
+              <LegendDetail
+                legendKey={activeKey}
+              />
+            </div>
           </div>
-          <div>
-            <LegendDetail
-              legendKey={activeKey}
-            />
-          </div>
-        </div>
-
         }
       </div>
-
       {service.status === 'error' && (
         <div>Error, the backend moved to the dark side.</div>
       )}
