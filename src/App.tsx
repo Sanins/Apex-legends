090df0afc4e7ec5b8c9c90ec092ae0b0e970b3ad
Robert Sanins <rob.sanins@lovehoney.co.uk>
2019-08-16 16:01:45 +0100

    Footer sticks to the bottom AND Home included

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import LegendList from './components/Legends/LegendList';
+import Home from './components/Home/Home';
 
 const routes = [
     {
@@ -11,26 +12,36 @@ const routes = [
         exact: true,
         main: () => <LegendList/>,
     },
+    {
+        path: "/",
+        exact: true,
+        main: () => <Home/>,
+    },
 ];
 
 export default function App() {
 
     return (
         <Router>
-            <div className='header-container'>
-                <Header/>
+            <div className="page-container">
+                <div className="content-wrap">
+                    <div className='header-container'>
+                        <Header/>
+                    </div>
+                    <>
+                        {routes.map((route, index) => (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                exact={route.exact}
+                                component={route.main}
+                            />
+                        ))}
+                    </>
+                </div>
+                <Footer/>
             </div>
-            <>
-                {routes.map((route, index) => (
-                    <Route
-                        key={index}
-                        path={route.path}
-                        exact={route.exact}
-                        component={route.main}
-                    />
-                ))}
-            </>
-            <Footer/>
+            
         </Router>
     )
 }
