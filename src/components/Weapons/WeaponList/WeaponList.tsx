12dc57148b9ded256c8642465768f6bfc89229d3
Robert Sanins <rob.sanins@lovehoney.co.uk>
2019-12-16 11:45:55 +0000

    Weapon list maps active values to the keys correctly

--- src/components/Weapons/WeaponList/WeaponList.tsx
+++ src/components/Weapons/WeaponList/WeaponList.tsx
@@ -2,26 +2,52 @@ import React from "react";
 import ApexLegendsByUrlService from './../../Services/ApexLegendsByUrlService';
 import { WeaponListProps } from "../../Types/Types";
 import './WeaponList.scss';
+import clsx from "clsx";
 
 
 const WeaponList: React.FC<{}> = () => {
 
   const [serviceValue, setServiceValue] = React.useState('https://www.apexdata.gg/api/OA1rrltgyhMHfknCo2dbFQtt/weapons/assault-rifles.json');
+  const [activeWeaponTypeValue, setActiveWeaponTypeValue] = React.useState(0);
+  const [activeWeaponValue, setActiveWeaponValue] = React.useState(0);
 
-  const handleChange = (param:number) => (e:any) => {
+  const handleWeaponChange = (param: number) => (e: any) => {
     switch (param) {
       case 0:
+        return setActiveWeaponValue(0);
+      case 1:
+        return setActiveWeaponValue(1);
+      case 2:
+        return setActiveWeaponValue(2);
+      case 3:
+        return setActiveWeaponValue(3);
+      case 4:
+        return setActiveWeaponValue(4);
+      case 5:
+        return setActiveWeaponValue(5);
+    };
+  };
+
+  const handleChange = (param: number) => (e: any) => {
+    switch (param) {
+      case 0:
+        setActiveWeaponTypeValue(0);
         return setServiceValue('https://www.apexdata.gg/api/OA1rrltgyhMHfknCo2dbFQtt/weapons/assault-rifles.json');
       case 1:
+        setActiveWeaponTypeValue(1);
         return setServiceValue('https://www.apexdata.gg/api/OA1rrltgyhMHfknCo2dbFQtt/weapons/sub-machine-guns.json');
       case 2:
-          return setServiceValue('weapons/light-machine-guns');
+        setActiveWeaponTypeValue(2);
+        return setServiceValue('https://www.apexdata.gg/api/OA1rrltgyhMHfknCo2dbFQtt/weapons/light-machine-guns');
       case 3:
-          return setServiceValue('weapons/shotguns');
+        setActiveWeaponTypeValue(3);
+        return setServiceValue('https://www.apexdata.gg/api/OA1rrltgyhMHfknCo2dbFQtt/weapons/shotguns');
       case 4:
-          return setServiceValue('weapons/sniper-rifles');
+        setActiveWeaponTypeValue(4);
+        return setServiceValue('https://www.apexdata.gg/api/OA1rrltgyhMHfknCo2dbFQtt/weapons/sniper-rifles');
       case 5:
-          return setServiceValue('weapons/pistols');
+        setActiveWeaponTypeValue(5);
+        return setServiceValue('https://www.apexdata.gg/api/OA1rrltgyhMHfknCo2dbFQtt/weapons/pistols');
     };
   };
 
@@ -41,14 +67,12 @@ const WeaponList: React.FC<{}> = () => {
           <div className='weapon-list__information'>
             <div className='weapon-list-filters'>
               <div className='weapon-list-filters__list'>
-                <ul>
-                  <li onClick={handleChange(0)} className='weapon-list-filters__list-item weapon-list-filters__list-item--active'>Assault Rifles</li>
-                  <li onClick={handleChange(1)} className='weapon-list-filters__list-item'>Sub Machine Guns</li>
-                  <li onClick={handleChange(2)} className='weapon-list-filters__list-item'>Light Machine Guns</li>
-                  <li onClick={handleChange(3)} className='weapon-list-filters__list-item'>Shotguns</li>
-                  <li onClick={handleChange(4)} className='weapon-list-filters__list-item'>Sniper Rifles</li>
-                  <li onClick={handleChange(5)} className='weapon-list-filters__list-item'>Pistols</li>
-                </ul>
+                <a onClick={handleChange(0)} className={clsx('weapon-list-filters__list-item', activeWeaponTypeValue === 0 && 'weapon-list-filters__list-item__active')}>Assault Rifles</a>
+                <a onClick={handleChange(1)} className={clsx('weapon-list-filters__list-item', activeWeaponTypeValue === 1 && 'weapon-list-filters__list-item__active')}>Sub Machine Guns</a>
+                <a onClick={handleChange(2)} className={clsx('weapon-list-filters__list-item', activeWeaponTypeValue === 2 && 'weapon-list-filters__list-item__active')}>Light Machine Guns</a>
+                <a onClick={handleChange(3)} className={clsx('weapon-list-filters__list-item', activeWeaponTypeValue === 3 && 'weapon-list-filters__list-item__active')}>Shotguns</a>
+                <a onClick={handleChange(4)} className={clsx('weapon-list-filters__list-item', activeWeaponTypeValue === 4 && 'weapon-list-filters__list-item__active')}>Sniper Rifles</a>
+                <a onClick={handleChange(5)} className={clsx('weapon-list-filters__list-item', activeWeaponTypeValue === 5 && 'weapon-list-filters__list-item__active')}>Pistols</a>
               </div>
               <div className='weapon-list-filters__dropdown'>
                 <div className='weapon-list-filters__dropdown__title'>
@@ -82,16 +106,24 @@ const WeaponList: React.FC<{}> = () => {
                 </div>
                 <button>Filter</button>
               </div>
-          </div>
-            <div className='weapon-list-of-weapons'>
-              {service.payload.map((weaponCategories: WeaponListProps) => (
+            </div>
+            <div className='weapon-list-filters__list'>
+              {service.payload.map((weaponCategories: WeaponListProps, key: number) => (
                 <>
-                  <p>{weaponCategories.name}</p>
+                  <a key={key} onClick={handleWeaponChange(key)} className={clsx('weapon-list-filters__list-item', activeWeaponValue === key && 'weapon-list-filters__list-item__active')}>{weaponCategories.name}</a>
                 </>
               ))}
             </div>
-            <div className='weapon-list-data'>
-              <p>Dummy data</p>
+            <div>
+              {service.payload.map((weaponCategories: WeaponListProps, key: number) => (
+                <div className='weapon-list-data'>
+                  {key === activeWeaponValue &&
+                    <div>
+                      {weaponCategories.damage}
+                    </div>
+                  }
+                </div>
+              ))}
             </div>
           </div>
         </div>
