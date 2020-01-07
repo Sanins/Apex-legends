a92222e8121f40a18bb068802fe5befddd7a976d
Robert Sanins <rob.sanins@lovehoney.co.uk>
2020-01-07 10:28:28 +0000

    barChart tests

--- src/components/Common/BarChart/BarChart.spec.tsx
+++ src/components/Common/BarChart/BarChart.spec.tsx
@@ -1,13 +1,21 @@
 /* eslint-disable no-undef */
 import React from "react";
-import { mount, shallow } from "enzyme";
+import { mount, render } from "enzyme";
 import BarChart from "./BarChart";
 import toJson from "enzyme-to-json";
+import { act } from "react-dom/test-utils";
+
+jest.useFakeTimers();
 
 describe("BarChart", () => {
-	it("should fetch data on mount", () => {
-		const wrapper = shallow(<BarChart dataValue={50} dataTitle="Headshot dps" />);
-		wrapper.update();
-		expect(toJson(wrapper)).toMatchSnapshot();
+	it("should bring back the width value 12.50%", () => {
+		const wrapper = mount(<BarChart dataValue={50} dataTitle="Headshot dps" />);
+		act(() => {
+			// eslint-disable-next-line no-magic-numbers
+			jest.advanceTimersByTime(1000);
+			wrapper.update();
+			const svg = wrapper.find("bar-chart__svg__live-data");
+			expect(svg.find("width").length).toEqual("12.5%");
+		});
 	});
 });
